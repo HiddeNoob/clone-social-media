b9c35b1910ad2df679a45ccb339581dbd9a29535
Alihan Gündoğdu <141227519+HiddeNoob@users.noreply.github.com>
2024-09-16 19:13:43 +0300

    some adjustments

--- routes/backend/api/image.js
+++ routes/backend/api/image.js
@@ -9,7 +9,7 @@ image.post('/image',checkAuthenticate,async (req, res) => {
     let photo = req.body
     const blob = await (await fetch(photo)).blob();
     let arrayBuffer = await blob.arrayBuffer()
-    let resizedBuffer = await sharp(arrayBuffer).resize({width: 100, height: 100}).toBuffer(); // resize image to decrease the size
+    let resizedBuffer = await sharp(arrayBuffer).resize({width: 75, height: 75}).toBuffer(); // resize image to decrease the size
     let response = await uploadImage(resizedBuffer,req.user.username,"png");
     res.sendStatus(response == true ? 200 : 400);
 });
